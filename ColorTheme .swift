b68ac1d6ee2d97c7f20f3fcac1b340315f9d75de
Jacquese Whitson <jacqwhitsonappledev@gmail.com>
2024-03-07 22:36:54 -0500

    updated map and descirpiton UI on deatil view, removed the first and last nae option on signup page

---
+++ ColorTheme .swift
@@ -0,0 +1,8 @@
+//
+//  ColorTheme .swift
+//  Moonshot
+//
+//  Created by Jacquese Whitson on 8/5/23.
+//
+
+import Foundation
---
+++ CreatedDivider.swift
@@ -0,0 +1,20 @@
+//
+//  CreatedDivider.swift
+//  Moonshot
+//
+//  Created by Jacquese Whitson on 8/13/23.
+//
+
+import SwiftUI
+
+struct CreatedDivider: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CreatedDivider_Previews: PreviewProvider {
+    static var previews: some View {
+        CreatedDivider()
+    }
+}
